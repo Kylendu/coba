ee2563b8ec63c10ffcf283ec88695e89dfe721bd
Imamgg <imamgenerasiolar@gmail.com>
2024-11-28 18:48:06 +0700

    modified contact

--- src/pages/components/fragments/contact/contactForm.jsx
+++ src/pages/components/fragments/contact/contactForm.jsx
@@ -5,31 +5,37 @@ const ContactForm = ({ onSubmit }) => {
       <div className="text-[#4D5BCE]">
         <span className="text-[#c161e4]">function </span>
         <span className="text-[#5565E8]">sendMessage</span>
-        <span className="text-white">() {"{"}</span>
+        <span className="text-slate-300">() {"{"}</span>
       </div>
       <form onSubmit={onSubmit} className="ml-4 space-y-4">
         <input
           type="text"
           name="name"
-          placeholder="name = 'Your Name'"
-          className="w-full bg-[#1E1E1E] border border-[#4D5BCE] p-2 rounded text-white"
+          placeholder="Your Name"
+          required
+          className="w-full bg-transparent border border-[#4D5BCE] p-2 rounded text-slate-300"
         />
         <input
           type="email"
           name="email"
-          placeholder="email = 'your@email.com'"
-          className="w-full bg-[#1E1E1E] border border-[#4D5BCE] p-2 rounded text-white"
+          required
+          placeholder="mail@example.com"
+          className="w-full bg-transparent border border-[#4D5BCE] p-2 rounded text-slate-300"
         />
         <textarea
           name="message"
-          placeholder="message = 'Your message here...'"
-          className="w-full bg-[#1E1E1E] border border-[#4D5BCE] p-2 rounded text-white h-32"
+          placeholder="Your message here..."
+          required
+          className="w-full bg-transparent border border-[#4D5BCE] p-2 rounded text-slate-300 h-32"
         ></textarea>
-        <button type="submit" className="bg-[#4D5BCE] text-white px-6 py-2 rounded hover:bg-[#3A4494]">
+        <button
+          type="submit"
+          className="bg-[#4D5BCE] text-slate-300 px-6 py-2 rounded hover:bg-[#3A4494]"
+        >
           Execute
         </button>
       </form>
-      <span className="text-white">{"}"}</span>
+      <span className="text-slate-300">{"}"}</span>
     </div>
   );
 };
